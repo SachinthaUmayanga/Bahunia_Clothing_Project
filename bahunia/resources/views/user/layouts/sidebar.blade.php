ac2b8477671b114d230b1cb366d4fcfade761fbb
Sachintha Umayanga <128795701+SachinthaUmayanga@users.noreply.github.com>
2023-10-06 21:11:04 +0530

    Changes

--- bahunia/resources/views/user/layouts/sidebar.blade.php
+++ bahunia/resources/views/user/layouts/sidebar.blade.php
@@ -2,10 +2,10 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('user')}}">
-      <div class="sidebar-brand-icon rotate-n-15">
+      {{-- <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-laugh-wink"></i>
-      </div>
-      <div class="sidebar-brand-text mx-3">User</div>
+      </div> --}}
+      <div class="sidebar-brand-text mx-3">User Dashboard</div>
     </a>
 
     <!-- Divider -->

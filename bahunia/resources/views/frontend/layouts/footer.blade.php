a5d75dcb162678705739dba7f096f6284fdd9331
Sachintha Umayanga <128795701+SachinthaUmayanga@users.noreply.github.com>
2023-10-07 16:18:09 +0530

    Update footer.blade.php

--- bahunia/resources/views/frontend/layouts/footer.blade.php
+++ bahunia/resources/views/frontend/layouts/footer.blade.php
@@ -60,7 +60,7 @@
 								</ul>
 							</div>
 							<!-- End Single Widget -->
-							<div class="sharethis-inline-follow-buttons"></div>
+							{{-- <div class="sharethis-inline-follow-buttons"></div> --}}
 						</div>
 						<!-- End Single Widget -->
 					</div>

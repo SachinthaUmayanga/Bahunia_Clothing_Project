cffcb6a4b8d5684fb15c405b243272e98e3558cd
Sachintha Umayanga <128795701+SachinthaUmayanga@users.noreply.github.com>
2023-11-15 13:06:43 +0530

    Update index.blade.php

--- bahunia/resources/views/backend/index.blade.php
+++ bahunia/resources/views/backend/index.blade.php
@@ -230,7 +230,7 @@
                           padding: 10,
                           // Include a dollar sign in the ticks
                           callback: function(value, index, values) {
-                            return '$' + number_format(value);
+                            return 'Rs.' + number_format(value);
                           }
                         },
                         gridLines: {
@@ -262,7 +262,7 @@
                       callbacks: {
                         label: function(tooltipItem, chart) {
                           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-                          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+                          return datasetLabel + ': Rs.' + number_format(tooltipItem.yLabel);
                         }
                       }
                     }
